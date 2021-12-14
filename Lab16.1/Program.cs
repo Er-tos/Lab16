a5f337caa6e12c1d9cd0e3578e2912593727d67c
a.v.ershov <a.v.ershov@137_1>
2021-12-14 17:37:18 +0700

    Добавьте файлы проекта.

---
+++ Lab16.1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+using System.IO;
+
+namespace Lab16._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Запись информации в базу о товарах.");
+            Console.WriteLine("Введите количество товаров, которое вы хотите записать.");
+            int productCount = Convert.ToInt32(Console.ReadLine());
+            Product[] productArray = new Product[productCount];
+            for (int i = 0; i < productCount; i++)
+            {
+                Console.WriteLine("Введите код {0} товара.", i + 1);
+                int code = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите имя {0} товара.", i + 1);
+                string name = Console.ReadLine();
+                Console.WriteLine("Введите цену {0} товара.", i + 1);
+                double price = Convert.ToDouble(Console.ReadLine());
+                Product prod = new Product()
+                {
+                    ProductCode = code,
+                    ProductName = name,
+                    ProductPrice = price
+                };
+                productArray[i] = prod;
+            }
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                WriteIndented = true
+            };
+            string report = JsonSerializer.Serialize(productArray, options);
+            Console.WriteLine(report);
+            string pathToDir = "Example";
+            if (!Directory.Exists(pathToDir))
+            {
+                Directory.CreateDirectory(pathToDir);
+            }
+            string fileName = "Products.json";
+            string pathToFile = pathToDir + "/" + fileName;
+            if (!File.Exists(pathToFile))
+            {
+                File.Create(pathToFile);
+            }
+            using (StreamWriter sw = new StreamWriter(pathToFile, false))
+            Console.ReadKey();
+        }
+        class Product
+        {
+            public int ProductCode { get; set; }
+            public string ProductName { get; set; }
+            public double ProductPrice { get; set; }
+        }
+    }
+}
---
+++ Lab16.2/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
+using System.Threading.Tasks;
+
+namespace Lab16._2
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Получение информации из базы данных о товарах.");
+            string path = "Example/Products.json";
+            string line = "";
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("Файл не найден!");
+            }
+            else
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    line = sr.ReadToEnd();
+                }
+            }
+            line = line.Remove(0, 3);
+            line = line.Remove(line.Length - 5, 5);
+            string[] separatingStrings = { "}," };
+            string[] stringArray = line.Split(separatingStrings, System.StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < stringArray.Length - 1; i++)
+            {
+                stringArray[i] = stringArray[i] + "}";
+            }
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                WriteIndented = true
+            };
+            Product[] prodBase = new Product[stringArray.Length];
+            int num = 0;
+            foreach (string item in stringArray)
+            {
+                Product prod = JsonSerializer.Deserialize<Product>(item, options);
+                prodBase[num] = prod;
+                num++;
+            }
+            double productPriceMax = 0;
+            string productPriceMaxName = "";
+            foreach (Product item in prodBase)
+            {
+                Console.WriteLine("В базе имеется товар с кодом: {0}, называнием: {1} и стоимостью {2}.", item.ProductCode, item.ProductName, item.ProductPrice);
+                if (item.ProductPrice > productPriceMax)
+                {
+                    productPriceMax = item.ProductPrice;
+                    productPriceMaxName = item.ProductName;
+                }
+            }
+            Console.WriteLine();
+            Console.Write("Самый дорогой товар в базе - это {0}, его стоимость {1}.", productPriceMaxName, productPriceMax);
+            Console.ReadKey();
+        }
+        class Product
+        {
+            public int ProductCode { get; set; }
+            public string ProductName { get; set; }
+            public double ProductPrice { get; set; }
+        }
+    }
+}
